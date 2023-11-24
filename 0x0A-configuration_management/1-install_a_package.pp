05631bae575dbcb47e7ca767b642a57c68ef32bc
yung-wolf <edmundbansah@gmial.com>
2023-11-24 11:02:14 -0800

    fix II

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
 #install flask 2.1.0 using pip3
-package { 'Werkzeug':
+package {'python3-pip':
+  ensure => installed,
+}
+
+package {'Werkzeug':
   ensure => '2.2.2',
   provider => 'pip3',
+  require => Package['python3-pip'],
 }
