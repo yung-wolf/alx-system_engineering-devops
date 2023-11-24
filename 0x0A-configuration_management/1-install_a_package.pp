d94b3cdcb415e528402008722611eaa36394caaa
yung-wolf <edmundbansah@gmial.com>
2023-11-24 11:05:52 -0800

    fix II

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,8 +3,13 @@ package {'python3-pip':
   ensure => installed,
 }
 
-package {'Werkzeug':
-  ensure => '2.2.2',
+package {'flask':
+  ensure => '2.1.0',
   provider => 'pip3',
   require => Package['python3-pip'],
 }
+
+package { 'Werkzeug':
+  ensure   => '2.2.2',
+  provider => 'pip3',
+} 
